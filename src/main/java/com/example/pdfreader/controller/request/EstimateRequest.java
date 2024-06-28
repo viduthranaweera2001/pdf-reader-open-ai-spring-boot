00bad876a1a8ab371ca6fd76f1d07f60ec85a75e
viduthranaweera2001 <it21260056@sliit.lk>
2024-06-28 15:07:55 +0530

    remove getter setter

--- src/main/java/com/example/pdfreader/controller/request/EstimateRequest.java
+++ src/main/java/com/example/pdfreader/controller/request/EstimateRequest.java
@@ -5,12 +5,4 @@ import lombok.Data;
 @Data
 public class EstimateRequest {
     private String description;
-
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
 }
--- src/main/java/com/example/pdfreader/controller/response/EstimateResponse.java
+++ src/main/java/com/example/pdfreader/controller/response/EstimateResponse.java
@@ -7,28 +7,4 @@ public class EstimateResponse {
     private String timeEstimate;
     private String costEstimate;
     private String description;
-
-//    public String getTimeEstimate() {
-//        return timeEstimate;
-//    }
-//
-//    public void setTimeEstimate(String timeEstimate) {
-//        this.timeEstimate = timeEstimate;
-//    }
-//
-//    public String getCostEstimate() {
-//        return costEstimate;
-//    }
-//
-//    public void setCostEstimate(String costEstimate) {
-//        this.costEstimate = costEstimate;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
 }
